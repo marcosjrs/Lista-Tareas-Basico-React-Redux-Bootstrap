8b6893ac0a709704e1a6e35aa3338b3a5aeb5ca0
marcosjrs <marcosjrs80@gmail.com>
2017-11-16 20:35:26 +0100

    Instalamos bootstrap 3 como dependencia (antes lo hicimos con react-bootstrap) e importamos las css en el index.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 import App from './App';
 import * as tipos from './constantes';
