2c488990c790c3c42fff79fb9cfaf5469fc27b6e
marcosjrs <marcosjrs80@gmail.com>
2017-11-16 20:23:37 +0100

    Importamos constantes, creamos estado, reductores y store. ( Las acciones se crearán más adelante donde se 'disparen')

--- src/index.js
+++ src/index.js
@@ -1,8 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
 import './index.css';
 import App from './App';
+import * as tipos from './constantes';
 import registerServiceWorker from './registerServiceWorker';
+
+//Estado
+const estadoGlobal = {
+    tareas:[]
+};
+
+//Reductores 
+const tareasReductor = function(estado, accion){
+    const nuevoEstado = Object.assign({},estado);
+    let nuevasTareas;
+    switch (accion.type) {
+        case tipos.ADD:
+            nuevasTareas = nuevoEstado.tareas.concat({
+                id:accion.id,
+                texto:accion.texto,
+                hecho: accion.hecho
+            });
+            nuevoEstado.tareas = nuevasTareas;
+            return nuevoEstado;
+    
+        case tipos.DEL:
+            nuevasTareas = nuevoEstado.tareas.filter(function(item){ 
+                return (item.id+"" !== accion.id);
+            });
+            nuevoEstado.tareas = nuevasTareas;
+            return nuevoEstado;
+        case tipos.CHANGE:
+            nuevoEstado.tareas = estado.tareas.slice();
+            nuevoEstado.tareas.forEach(function(item){ 
+                if (item.id+"" === accion.id){
+                    item.hecho = !item.hecho;
+                }
+            });
+            return nuevoEstado;
+        default:
+         return estado;
+    }
+}
+
+
+//Store
+var store = createStore(tareasReductor, estadoGlobal);
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
